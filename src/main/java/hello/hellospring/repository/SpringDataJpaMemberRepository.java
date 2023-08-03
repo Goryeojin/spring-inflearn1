5884e8d63a276de7b2dadf0ffb0c4c3e7d6dcc93
ryeojin <ryeojin@biztalk.co.kr>
2023-08-03 20:31:54 +0900

    Aspect Oriented Programming 적용하여 공통 관심사와 핵심 관심사 분리하기

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -2,9 +2,11 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     @Override
